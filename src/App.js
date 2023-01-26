26a1e93e929449df8eb36b244ef520f8545a1011
maggie-mar <magdalena.martin2024@mauihigh.org>
2023-01-26 12:46:48 -1000

    finished calc

--- src/App.js
+++ src/App.js
@@ -3,78 +3,80 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [value, setVal] = useState(0);
-  const [prevVal, setPrevVal] = useState(0);
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
   const [op, setOp] = useState("");
-  const [ans, setAns] = useState(0);
+  const [ans, setAns] = useState("");
 
-  const handleClick = (e) => {
-    
-    
-    setAns(e.target.value);
-    if(value === "+" || value === "-" || value === "*" || value === "/"){
-      setPrevVal(prevVal + ans);
-      setOp(value);
-      setVal(0);
-    }
-
-    else if(value === "="){
-      switch(op){
-        case("+"):
-        console.log("add");
-        setAns(parseInt(prevVal) + parseInt(value));
-        break;
-
-        case("-"):
-        setAns(prevVal - value);
-        break;
-
-        case("*"):
-        setAns(prevVal * value);
-        break;
-
-        case("/"):
-        setAns(prevVal / value);
-        break;
-
-        default:
-          setAns(value);
-
-      }
+  
+  const handleNum = (e) => {
+    if(op === ""){
+      setInput1(input1 + e.target.value);
     }
     else{
-      setVal(value);
+      setInput2(input2 + e.target.value);
     }
+    
   }
+
+  const handleOp = (e) => {
+    setOp(e.target.value);
+  }
+
+  const calc = () => {
+    if(op === "+"){
+      setAns(parseInt(input1) + parseInt(input2));
+    }
+    else if(op === "-"){
+      setAns(parseInt(input1) - parseInt(input2));
+    }
+    else if(op === "*"){
+      setAns(parseInt(input1) * parseInt(input2));
+    }
+    else if(op === "/"){
+      setAns(parseInt(input1) / parseInt(input2));
+    }
+    setInput1("");
+    setInput2("");
+    setOp("");
+  }
+
+  const clear = () => {
+    setInput1("");
+    setInput2("");
+    setOp("");
+    setAns("");
+  }
+  
 
   return (
     <div>
       <div>
-        <input type="text" value={ans}></input>
+        <input type="text" value={input1 + " " + op + " " + input2 + " = " + ans}></input>
       </div>
       <div>
-        <button value="0" onClick= {handleClick}>0</button>
-        <button value="1" onClick={handleClick}>1</button>
-        <button value="2" onClick={handleClick}>2</button>
-        <button value="+" onClick={handleClick}>+</button>
+        <button value="0" onClick={handleNum}>0</button>
+        <button value="1" onClick={handleNum}>1</button>
+        <button value="2" onClick={handleNum}>2</button>
+        <button value="+" onClick={handleOp}>+</button>
       </div>
       <div>
-        <button value="3" onClick={handleClick}>3</button>
-        <button value="4" onClick={handleClick}>4</button>
-        <button value="5" onClick={handleClick}>5</button>
-        <button value="-" onClick={handleClick}>-</button>
+        <button value="3" onClick={handleNum}>3</button>
+        <button value="4" onClick={handleNum}>4</button>
+        <button value="5" onClick={handleNum}>5</button>
+        <button value="-" onClick={handleOp}>-</button>
       </div>
       <div>
-        <button value="6" onClick={handleClick}>6</button>
-        <button value="7" onClick={handleClick}>7</button>
-        <button value="8" onClick={handleClick}>8</button>
-        <button value="*" onClick={handleClick}>*</button>
+        <button value="6" onClick={handleNum}>6</button>
+        <button value="7" onClick={handleNum}>7</button>
+        <button value="8" onClick={handleNum}>8</button>
+        <button value="*" onClick={handleOp}>*</button>
       </div>
       <div>
-        <button value="9" onClick={handleClick}>9</button>
-        <button value="=" onClick={handleClick}>=</button>
-        <button value="AC" onClick={handleClick}>AC</button>
-        <button value="/" onClick={handleClick}>/</button>
+        <button value="9" onClick={handleNum}>9</button>
+        <button value="=" onClick={calc}>=</button>
+        <button value="AC" onClick={clear}>AC</button>
+        <button value="/" onClick={handleOp}>/</button>
       </div>
       
       
